5e1fdadc7974eee709d2e9b185192c0005291bb4
tibbi <tibor@kaputa.sk>
2016-12-11 00:33:47 +0100

    create a new module Library

--- samples/src/main/java/com/simplemobiletools/commons/samples/activities/MainActivity.java
+++ samples/src/main/java/com/simplemobiletools/commons/samples/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.simplemobiletools.commons;
+package com.simplemobiletools.commons.samples.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.simplemobiletools.commons.samples.R;
 
 public class MainActivity extends AppCompatActivity {
 
