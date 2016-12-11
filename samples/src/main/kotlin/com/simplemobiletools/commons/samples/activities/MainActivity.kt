d48625b4215d15419cf7d818be856933556c03b8
tibbi <tibor@kaputa.sk>
2016-12-11 11:12:39 +0100

    add kotlin

---
+++ samples/src/main/kotlin/com/simplemobiletools/commons/samples/activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.simplemobiletools.commons.samples.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import com.simplemobiletools.commons.samples.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
