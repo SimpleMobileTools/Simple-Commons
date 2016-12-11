cd83f3d485df8cd529cd1d1a4f69b897d1da556e
tibbi <tibor@kaputa.sk>
2016-12-11 19:23:43 +0100

    add some cursor extensions

---
+++ library/src/main/kotlin/com/simplemobiletools/commons/extensions/Cursor.kt
@@ -0,0 +1,9 @@
+package com.simplemobiletools.commons.extensions
+
+import android.database.Cursor
+
+fun Cursor.getStringValue(key: String) = getString(getColumnIndex(key))
+
+fun Cursor.getIntValue(key: String) = getInt(getColumnIndex(key))
+
+fun Cursor.getLongValue(key: String) = getLong(getColumnIndex(key))
