4c4bbf7faab2439dc24c09a4969869e1ba7ad1db
tibbi <tibor@kaputa.sk>
2016-12-14 23:06:39 +0100

    draw a frame around the background color picker

--- library/src/main/kotlin/com/simplemobiletools/commons/activities/CustomizationActivity.kt
+++ library/src/main/kotlin/com/simplemobiletools/commons/activities/CustomizationActivity.kt
@@ -1,6 +1,9 @@
 package com.simplemobiletools.commons.activities
 
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.view.View
 import com.simplemobiletools.commons.R
 import kotlinx.android.synthetic.main.activity_customization.*
 import yuku.ambilwarna.AmbilWarnaDialog
@@ -22,8 +25,22 @@ class CustomizationActivity : BaseSimpleActivity() {
 
     private fun setupColorsPickers() {
         customization_text_color.setBackgroundColor(baseConfig.textColor)
-        customization_background_color.setBackgroundColor(baseConfig.backgroundColor)
         customization_primary_color.setBackgroundColor(baseConfig.primaryColor)
+        customView(customization_background_color, baseConfig.backgroundColor, getContrastColor(baseConfig.backgroundColor))
+    }
+
+    fun customView(view: View, backgroundColor: Int, borderColor: Int) {
+        GradientDrawable().apply {
+            shape = GradientDrawable.RECTANGLE
+            setColor(backgroundColor)
+            setStroke(2, borderColor)
+            view.setBackgroundDrawable(this)
+        }
+    }
+
+    fun getContrastColor(color: Int): Int {
+        val y = (299 * Color.red(color) + 587 * Color.green(color) + 114 * Color.blue(color)) / 1000
+        return if (y >= 128) Color.BLACK else Color.WHITE
     }
 
     private fun pickTextColor() {
