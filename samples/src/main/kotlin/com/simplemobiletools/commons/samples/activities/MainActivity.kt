82531ff2c40334b1c0b962989d09134315cbfee5
tibbi <tibor@kaputa.sk>
2016-12-15 18:24:01 +0100

    move the icons into the drawable folder

--- samples/src/main/kotlin/com/simplemobiletools/commons/samples/activities/MainActivity.kt
+++ samples/src/main/kotlin/com/simplemobiletools/commons/samples/activities/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.simplemobiletools.commons.samples.activities
 
+import android.content.Intent
 import android.os.Bundle
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
+import com.simplemobiletools.commons.activities.CustomizationActivity
 import com.simplemobiletools.commons.samples.R
 
 class MainActivity : BaseSimpleActivity() {
@@ -9,5 +11,8 @@ class MainActivity : BaseSimpleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, CustomizationActivity::class.java))
+        finish()
     }
 }
