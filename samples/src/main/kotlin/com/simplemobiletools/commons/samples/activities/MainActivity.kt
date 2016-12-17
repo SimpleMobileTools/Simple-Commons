409bab91c02bd292e9afa2d3c62fd60f8e22e5f4
tibbi <tibor@kaputa.sk>
2016-12-17 11:22:23 +0100

    remove the initial activity call

--- samples/src/main/kotlin/com/simplemobiletools/commons/samples/activities/MainActivity.kt
+++ samples/src/main/kotlin/com/simplemobiletools/commons/samples/activities/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.simplemobiletools.commons.samples.activities
 
-import android.content.Intent
 import android.os.Bundle
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
-import com.simplemobiletools.commons.activities.CustomizationActivity
 import com.simplemobiletools.commons.samples.R
 
 class MainActivity : BaseSimpleActivity() {
@@ -11,8 +9,5 @@ class MainActivity : BaseSimpleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        startActivity(Intent(this, CustomizationActivity::class.java))
-        finish()
     }
 }
