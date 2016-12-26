bc441cf2d12869c32c6ff8eb547147fe5ffde889
tibbi <tibor@kaputa.sk>
2016-12-26 12:31:04 +0100

    add properties extensions

--- commons/src/main/kotlin/com/simplemobiletools/commons/extensions/File.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/extensions/File.kt
@@ -1,5 +1,6 @@
 package com.simplemobiletools.commons.extensions
 
+import android.graphics.BitmapFactory
 import android.media.MediaMetadataRetriever
 import java.io.File
 
@@ -34,4 +35,52 @@ fun File.getMimeType(default: String = ""): String {
     } catch (ignored: Exception) {
         default
     }
+}
+
+fun File.getDuration(): String {
+    val retriever = MediaMetadataRetriever()
+    retriever.setDataSource(path)
+    val time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
+    val timeInMs = java.lang.Long.parseLong(time)
+    return (timeInMs / 1000).toInt().getFormattedDuration()
+}
+
+fun File.getArtist(): String? {
+    val retriever = MediaMetadataRetriever()
+    retriever.setDataSource(path)
+    return retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST)
+}
+
+fun File.getAlbum(): String? {
+    val retriever = MediaMetadataRetriever()
+    retriever.setDataSource(path)
+    return retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM)
+}
+
+fun File.getVideoResolution(): String {
+    try {
+        val retriever = MediaMetadataRetriever()
+        retriever.setDataSource(path)
+        val width = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH).toInt()
+        val height = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT).toInt()
+        return "$width x $height ${getMPx(width, height)}"
+    } catch (ignored: Exception) {
+
+    }
+    return ""
+}
+
+fun File.getImageResolution(): String {
+    val options = BitmapFactory.Options()
+    options.inJustDecodeBounds = true
+    BitmapFactory.decodeFile(path, options)
+    val width = options.outWidth
+    val height = options.outHeight
+    return "$width x $height ${getMPx(width, height)}"
+}
+
+fun getMPx(width: Int, height: Int): String {
+    val px = width * height / 1000000.toFloat()
+    val rounded = Math.round(px * 10) / 10.toFloat()
+    return "(${rounded}MP)"
 }
