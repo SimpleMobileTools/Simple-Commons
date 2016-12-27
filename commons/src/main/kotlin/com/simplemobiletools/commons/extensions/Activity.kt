04e052f7c322a03a34e7bc1175656aa127a6477f
tibbi <tibor@kaputa.sk>
2016-12-27 22:19:21 +0100

    adding some extensions and text sizes

--- commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Activity.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Activity.kt
@@ -1,7 +1,12 @@
 package com.simplemobiletools.commons.extensions
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
 import com.simplemobiletools.commons.dialogs.WhatsNewDialog
 import com.simplemobiletools.commons.dialogs.WritePermissionDialog
@@ -26,4 +31,15 @@ fun BaseSimpleActivity.checkWhatsNew(releases: List<Release>) {
 
     if (newReleases.isNotEmpty())
         WhatsNewDialog(this, newReleases)
+}
+
+fun Activity.hideKeyboard() {
+    val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow((currentFocus ?: View(this)).windowToken, 0)
+    window!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+}
+
+fun Activity.showKeyboard(et: EditText) {
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(et, InputMethodManager.SHOW_IMPLICIT)
 }
