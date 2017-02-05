511a3e79e82d83de9f5c27b49604119f9f4cab80
tibbi <tibor@kaputa.sk>
2017-02-05 10:53:00 +0100

    some extension corrections

--- commons/src/main/kotlin/com/simplemobiletools/commons/extensions/View.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/extensions/View.kt
@@ -6,10 +6,10 @@ fun View.beInvisibleIf(beInvisible: Boolean) = if (beInvisible) beInvisible() el
 
 fun View.beVisibleIf(beVisible: Boolean) = if (beVisible) beVisible() else beGone()
 
-fun View.beGoneIf(beGone: Boolean) = if (beGone) beGone() else beVisible()
+fun View.beGoneIf(beGone: Boolean) = beVisibleIf(!beGone)
 
-fun View.beInvisible() = { visibility = View.INVISIBLE }
+fun View.beInvisible() { visibility = View.INVISIBLE }
 
-fun View.beVisible() = { visibility = View.VISIBLE }
+fun View.beVisible() { visibility = View.VISIBLE }
 
-fun View.beGone() = { visibility = View.GONE }
+fun View.beGone() { visibility = View.GONE }
