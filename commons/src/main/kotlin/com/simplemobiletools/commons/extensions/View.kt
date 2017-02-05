c738abcf56956fe8981eccef9ee755c1a87b2f6e
tibbi <tibor@kaputa.sk>
2017-02-05 10:40:22 +0100

    add an extension for View.INVISIBLE

--- commons/src/main/kotlin/com/simplemobiletools/commons/extensions/View.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/extensions/View.kt
@@ -2,9 +2,13 @@ package com.simplemobiletools.commons.extensions
 
 import android.view.View
 
-fun View.beVisibleIf(beVisible: Boolean) = if (beVisible) visibility = View.VISIBLE else visibility = View.GONE
+fun View.beInvisibleIf(beInvisible: Boolean) = if (beInvisible) beInvisible() else beVisible()
+
+fun View.beVisibleIf(beVisible: Boolean) = if (beVisible) beVisible() else beGone()
 
 fun View.beGoneIf(beGone: Boolean) = if (beGone) beGone() else beVisible()
+
+fun View.beInvisible() = { visibility = View.INVISIBLE }
 
 fun View.beVisible() = { visibility = View.VISIBLE }
 
