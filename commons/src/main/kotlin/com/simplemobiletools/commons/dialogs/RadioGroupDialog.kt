d3bbd354792d3ae0a0737eca403aabd165be1780
tibbi <tibor@kaputa.sk>
2017-03-09 23:41:13 +0100

    add a dialog for simple radiogroups

---
+++ commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/RadioGroupDialog.kt
@@ -0,0 +1,42 @@
+package com.simplemobiletools.commons.dialogs
+
+import android.app.Activity
+import android.support.v7.app.AlertDialog
+import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import com.simplemobiletools.commons.R
+import com.simplemobiletools.commons.extensions.setupDialogStuff
+import com.simplemobiletools.commons.models.RadioItem
+import kotlinx.android.synthetic.main.dialog_radio_group.view.*
+
+class RadioGroupDialog(val activity: Activity, val items: Array<RadioItem>, val checkedItemId: Int, val callback: (newValue: Int) -> Unit) :
+        AlertDialog.Builder(activity), RadioGroup.OnCheckedChangeListener {
+    val dialog: AlertDialog?
+
+    init {
+        val view = activity.layoutInflater.inflate(R.layout.dialog_radio_group, null)
+        view.dialog_radio_group.apply {
+            setOnCheckedChangeListener(this@RadioGroupDialog)
+            val resources = activity.resources
+
+            items.map {
+                (activity.layoutInflater.inflate(R.layout.radio_button, null) as RadioButton).apply {
+                    text = resources.getString(it.titleId)
+                    isChecked = it.id == checkedItemId
+                    id = it.id
+                }
+            }.forEach { addView(it, RadioGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)) }
+        }
+
+        dialog = AlertDialog.Builder(activity)
+                .create().apply {
+            activity.setupDialogStuff(view, this)
+        }
+    }
+
+    override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+        callback.invoke(items[checkedId].id)
+        dialog?.dismiss()
+    }
+}
