4c5852090147c35384fb281a8ddb820feb232398
tibbi <tibor@kaputa.sk>
2017-03-10 21:12:32 +0100

    use an arraylist at RadioGroupDialog constructor

--- commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/RadioGroupDialog.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/RadioGroupDialog.kt
@@ -9,33 +9,39 @@ import com.simplemobiletools.commons.R
 import com.simplemobiletools.commons.extensions.setupDialogStuff
 import com.simplemobiletools.commons.models.RadioItem
 import kotlinx.android.synthetic.main.dialog_radio_group.view.*
+import java.util.*
 
-class RadioGroupDialog(val activity: Activity, val items: Array<RadioItem>, val checkedItemId: Int, val callback: (newValue: Int) -> Unit) :
+class RadioGroupDialog(val activity: Activity, val items: ArrayList<RadioItem>, val checkedItemId: Int, val callback: (newValue: Int) -> Unit) :
         AlertDialog.Builder(activity), RadioGroup.OnCheckedChangeListener {
     val dialog: AlertDialog?
+    var wasInit = false
 
     init {
         val view = activity.layoutInflater.inflate(R.layout.dialog_radio_group, null)
         view.dialog_radio_group.apply {
             setOnCheckedChangeListener(this@RadioGroupDialog)
 
-            items.map {
-                (activity.layoutInflater.inflate(R.layout.radio_button, null) as RadioButton).apply {
-                    text = it.title
-                    isChecked = it.id == checkedItemId
-                    id = it.id
+            for (i in 0..items.size - 1) {
+                val radioButton = (activity.layoutInflater.inflate(R.layout.radio_button, null) as RadioButton).apply {
+                    text = items[i].title
+                    isChecked = items[i].id == checkedItemId
+                    id = i
                 }
-            }.forEach { addView(it, RadioGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)) }
+                addView(radioButton, RadioGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+            }
         }
 
         dialog = AlertDialog.Builder(activity)
                 .create().apply {
             activity.setupDialogStuff(view, this)
         }
+        wasInit = true
     }
 
     override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
-        callback.invoke(items[checkedId].id)
-        dialog?.dismiss()
+        if (wasInit) {
+            callback.invoke(items[checkedId].id)
+            dialog?.dismiss()
+        }
     }
 }
