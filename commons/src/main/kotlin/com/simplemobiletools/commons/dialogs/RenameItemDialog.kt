827fe6d7db7d53643a2786870562d330fadcdde2
tibbi <tibor@kaputa.sk>
2017-03-14 22:44:56 +0100

    use a shared RenameItemDialog for both files and folders

---
+++ commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/RenameItemDialog.kt
@@ -0,0 +1,103 @@
+package com.simplemobiletools.commons.dialogs
+
+import android.support.v7.app.AlertDialog
+import android.view.WindowManager
+import com.simplemobiletools.commons.R
+import com.simplemobiletools.commons.activities.BaseSimpleActivity
+import com.simplemobiletools.commons.extensions.*
+import kotlinx.android.synthetic.main.dialog_rename_item.view.*
+import java.io.File
+import java.util.*
+
+class RenameItemDialog(val activity: BaseSimpleActivity, val path: String, val callback: (newPath: String) -> Unit) {
+    init {
+        val file = File(path)
+        val fullName = file.name
+        val dotAt = fullName.lastIndexOf(".")
+        var name = fullName
+
+        val view = activity.layoutInflater.inflate(R.layout.dialog_rename_item, null).apply {
+            if (dotAt > 0 && !file.isDirectory) {
+                name = fullName.substring(0, dotAt)
+                val extension = fullName.substring(dotAt + 1)
+                rename_item_extension.setText(extension)
+            } else {
+                rename_item_extension_label.beGone()
+                rename_item_extension.beGone()
+            }
+
+            rename_item_name.setText(name)
+            rename_item_path.text = activity.humanizePath(file.parent) + "/"
+        }
+
+        AlertDialog.Builder(activity)
+                .setPositiveButton(R.string.ok, null)
+                .setNegativeButton(R.string.cancel, null)
+                .create().apply {
+            window!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+            activity.setupDialogStuff(view, this, R.string.rename)
+            getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener({
+                var newName = view.rename_item_name.value
+                val newExtension = view.rename_item_extension.value
+
+                if (newName.isEmpty()) {
+                    activity.toast(R.string.empty_name)
+                    return@setOnClickListener
+                }
+
+                if (!newName.isAValidFilename()) {
+                    activity.toast(R.string.invalid_name)
+                    return@setOnClickListener
+                }
+
+                val updatedFiles = ArrayList<File>()
+                updatedFiles.add(file)
+                if (!newExtension.isEmpty())
+                    newName += ".$newExtension"
+
+                val newFile = File(file.parent, newName)
+                if (newFile.exists()) {
+                    activity.toast(R.string.name_taken)
+                    return@setOnClickListener
+                }
+
+                updatedFiles.add(newFile)
+                if (activity.needsStupidWritePermissions(file.absolutePath)) {
+                    activity.handleSAFDialog(file) {
+                        val document = activity.getFileDocument(file.absolutePath, activity.baseConfig.treeUri) ?: return@handleSAFDialog
+                        if (document.canWrite())
+                            document.renameTo(newName)
+                        sendSuccess(updatedFiles)
+                        dismiss()
+                    }
+                } else if (file.renameTo(newFile)) {
+                    updatedFiles[1].setLastModified(System.currentTimeMillis())
+                    sendSuccess(updatedFiles)
+                    dismiss()
+                } else {
+                    activity.toast(R.string.unknown_error_occurred)
+                }
+            })
+        }
+    }
+
+    private fun sendSuccess(updatedFiles: ArrayList<File>) {
+        activity.apply {
+            if (updatedFiles[1].isDirectory) {
+                val files = updatedFiles[1].listFiles()
+                if (files != null) {
+                    for (file in files) {
+                        scanPath(file.absolutePath) {
+                            callback(updatedFiles[1].absolutePath)
+                        }
+                        break
+                    }
+                }
+            } else {
+                scanFiles(updatedFiles) {
+                    callback(updatedFiles[1].absolutePath)
+                }
+            }
+        }
+    }
+}
