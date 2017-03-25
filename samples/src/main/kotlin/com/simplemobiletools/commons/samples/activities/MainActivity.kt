41b8f1745af379363121a48fc0014b480735311a
tibbi <tibor@kaputa.sk>
2017-03-25 11:27:18 +0100

    update version to 2.14.2

--- samples/src/main/kotlin/com/simplemobiletools/commons/samples/activities/MainActivity.kt
+++ samples/src/main/kotlin/com/simplemobiletools/commons/samples/activities/MainActivity.kt
@@ -2,7 +2,6 @@ package com.simplemobiletools.commons.samples.activities
 
 import android.os.Bundle
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
-import com.simplemobiletools.commons.dialogs.FilePickerDialog
 import com.simplemobiletools.commons.extensions.storeStoragePaths
 import com.simplemobiletools.commons.samples.R
 
@@ -12,8 +11,5 @@ class MainActivity : BaseSimpleActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         storeStoragePaths()
-        FilePickerDialog(this) {
-
-        }
     }
 }
