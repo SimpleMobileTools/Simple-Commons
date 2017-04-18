abf78bf215b37ca861475bc0774afb56428e9a6b
tibbi <tibor@kaputa.sk>
2017-04-18 19:21:17 +0200

    add handling for copy/move in base simple activity

--- commons/src/main/kotlin/com/simplemobiletools/commons/activities/BaseSimpleActivity.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/activities/BaseSimpleActivity.kt
@@ -8,27 +8,25 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Build
-import android.os.Bundle
 import android.provider.DocumentsContract
+import android.support.v4.util.Pair
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import com.simplemobiletools.commons.R
-import com.simplemobiletools.commons.extensions.baseConfig
-import com.simplemobiletools.commons.extensions.isShowingSAFDialog
-import com.simplemobiletools.commons.extensions.toast
+import com.simplemobiletools.commons.asynctasks.CopyMoveTask
+import com.simplemobiletools.commons.extensions.*
 import com.simplemobiletools.commons.helpers.APP_LICENSES
 import com.simplemobiletools.commons.helpers.APP_NAME
 import com.simplemobiletools.commons.helpers.APP_VERSION_NAME
 import com.simplemobiletools.commons.helpers.OPEN_DOCUMENT_TREE
 import java.io.File
+import java.util.*
 
 open class BaseSimpleActivity : AppCompatActivity() {
+    var copyMoveCallback: (() -> Unit)? = null
+
     companion object {
         var funAfterPermission: (() -> Unit)? = null
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
     }
 
     override fun onResume() {
@@ -128,6 +126,74 @@ open class BaseSimpleActivity : AppCompatActivity() {
         } else {
             callback()
             false
+        }
+    }
+
+    protected fun copyMoveFilesTo(files: ArrayList<File>, source: String, destination: String, isCopyOperation: Boolean, callback: () -> Unit) {
+        if (source == destination) {
+            toast(R.string.source_and_destination_same)
+            return
+        }
+
+        val destinationFolder = File(destination)
+        if (!destinationFolder.exists()) {
+            toast(R.string.invalid_destination)
+            return
+        }
+
+        if (files.size == 1) {
+            if (File(destinationFolder.absolutePath, files[0].name).exists()) {
+                toast(R.string.name_taken)
+                return
+            }
+        }
+
+        handleSAFDialog(destinationFolder) {
+            copyMoveCallback = callback
+            if (isCopyOperation) {
+                toast(R.string.copying)
+                val pair = Pair<ArrayList<File>, File>(files, destinationFolder)
+                CopyMoveTask(this, isCopyOperation, true, copyMoveListener).execute(pair)
+            } else {
+                if (isPathOnSD(source) || isPathOnSD(destinationFolder.absolutePath)) {
+                    handleSAFDialog(files[0]) {
+                        toast(R.string.moving)
+                        val pair = Pair<ArrayList<File>, File>(files, destinationFolder)
+                        CopyMoveTask(this, isCopyOperation, true, copyMoveListener).execute(pair)
+                    }
+                } else {
+                    val updatedFiles = ArrayList<File>(files.size * 2)
+                    updatedFiles.addAll(files)
+                    for (file in files) {
+                        val newFile = File(destinationFolder, file.name)
+                        if (!newFile.exists() && file.renameTo(newFile))
+                            updatedFiles.add(newFile)
+                    }
+
+                    scanFiles(updatedFiles) {
+                        runOnUiThread {
+                            copyMoveListener.copySucceeded(true, files.size * 2 == updatedFiles.size)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    protected val copyMoveListener = object : CopyMoveTask.CopyMoveListener {
+        override fun copySucceeded(copyOnly: Boolean, copiedAll: Boolean) {
+            if (copyOnly) {
+                toast(if (copiedAll) R.string.copying_success else R.string.copying_success_partial)
+            } else {
+                toast(if (copiedAll) R.string.moving_success else R.string.moving_success_partial)
+            }
+            copyMoveCallback?.invoke()
+            copyMoveCallback = null
+        }
+
+        override fun copyFailed() {
+            toast(R.string.copy_move_failed)
+            copyMoveCallback = null
         }
     }
 }
