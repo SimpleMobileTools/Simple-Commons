d1f8421c65ccc71e455e2e9ce470a103c0c2cf96
tibbi <tibor@kaputa.sk>
2017-06-25 23:49:49 +0200

    be incasesensitive when checking sd card path

--- commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Context-storage.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Context-storage.kt
@@ -28,7 +28,7 @@ fun Context.getSDCardPath(): String {
     }
 
     val directories = getStorageDirectories().filter { it.trimEnd('/') != internalStoragePath }
-    val sdCardPath = directories.firstOrNull { !physicalPaths.contains(it.trimEnd('/')) } ?: directories.firstOrNull() ?: ""
+    val sdCardPath = directories.firstOrNull { !physicalPaths.contains(it.toLowerCase().trimEnd('/')) } ?: directories.firstOrNull() ?: ""
     return sdCardPath.trimEnd('/')
 }
 
