38cf6398a751767859fa7574575ac6777d292850
tibbi <tibor@kaputa.sk>
2017-11-12 12:08:04 +0100

    adding an adapter helper interface

---
+++ commons/src/main/kotlin/com/simplemobiletools/commons/interfaces/MyAdapterListener.kt
@@ -0,0 +1,9 @@
+package com.simplemobiletools.commons.interfaces
+
+import java.util.*
+
+interface MyAdapterListener {
+    fun toggleItemSelectionAdapter(select: Boolean, position: Int)
+
+    fun getSelectedPositions(): HashSet<Int>
+}
