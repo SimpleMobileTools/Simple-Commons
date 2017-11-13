1f4810cc8d609c4d542bd2005b90f7757f444b5f
tibbi <tibor@kaputa.sk>
2017-11-13 21:19:15 +0100

    add an itemLongClicked function to MyAdapterListener interface

--- commons/src/main/kotlin/com/simplemobiletools/commons/interfaces/MyAdapterListener.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/interfaces/MyAdapterListener.kt
@@ -6,4 +6,6 @@ interface MyAdapterListener {
     fun toggleItemSelectionAdapter(select: Boolean, position: Int)
 
     fun getSelectedPositions(): HashSet<Int>
+
+    fun itemLongClicked(position: Int)
 }
