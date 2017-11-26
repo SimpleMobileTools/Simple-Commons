6ee3a8f290dc6e9b0dbd02e63fd9b88fcd68bee6
tibbi <tibor@kaputa.sk>
2017-11-26 22:29:31 +0100

    adding a LineColorPicker view

---
+++ commons/src/main/kotlin/com/simplemobiletools/commons/views/LineColorPicker.kt
@@ -0,0 +1,84 @@
+package com.simplemobiletools.commons.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.widget.LinearLayout
+import com.simplemobiletools.commons.R
+import com.simplemobiletools.commons.extensions.onGlobalLayout
+import com.simplemobiletools.commons.interfaces.LineColorPickerListener
+import java.util.*
+
+class LineColorPicker(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+    private var colorsCount = 0
+    private var pickerWidth = 0
+    private var stripeWidth = 0
+    private var unselectedMargin = 0
+    private var lastColorIndex = -1
+    private var colors = ArrayList<Int>()
+
+    var listener: LineColorPickerListener? = null
+
+    init {
+        unselectedMargin = context.resources.getDimension(R.dimen.line_color_picker_margin).toInt()
+        onGlobalLayout {
+            if (pickerWidth == 0) {
+                pickerWidth = width
+
+                if (colorsCount != 0)
+                    stripeWidth = width / colorsCount
+            }
+        }
+        orientation = LinearLayout.HORIZONTAL
+
+        setOnTouchListener { view, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_MOVE -> {
+                    if (pickerWidth != 0 && stripeWidth != 0) {
+                        touchAt(motionEvent.x.toInt())
+                    }
+                }
+            }
+            true
+        }
+    }
+
+    fun updateColors(colors: ArrayList<Int>) {
+        this.colors = colors
+        colorsCount = colors.size
+        if (pickerWidth != 0) {
+            stripeWidth = pickerWidth / colorsCount
+        }
+
+        removeAllViews()
+        val inflater = LayoutInflater.from(context)
+        colors.forEach {
+            inflater.inflate(R.layout.empty_image_view, this, false).apply {
+                setBackgroundColor(it)
+                addView(this)
+            }
+        }
+    }
+
+    private fun touchAt(touchX: Int) {
+        val colorIndex = touchX / stripeWidth
+        val index = Math.max(0, Math.min(colorIndex, colorsCount - 1))
+        if (lastColorIndex != index) {
+            updateItemMargin(lastColorIndex, true)
+            lastColorIndex = index
+            updateItemMargin(index, false)
+            listener?.colorChanged(index)
+        }
+    }
+
+    private fun updateItemMargin(index: Int, addMargin: Boolean) {
+        getChildAt(index)?.apply {
+            (layoutParams as LinearLayout.LayoutParams).apply {
+                topMargin = if (addMargin) unselectedMargin else 0
+                bottomMargin = if (addMargin) unselectedMargin else 0
+            }
+            requestLayout()
+        }
+    }
+}
