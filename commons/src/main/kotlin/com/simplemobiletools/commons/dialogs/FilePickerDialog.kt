246a83decebe82fbf919cf2f6535a1e83657e696
tibbi <tibor@kaputa.sk>
2017-12-06 23:08:01 +0100

    updating some imports

--- commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/FilePickerDialog.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/FilePickerDialog.kt
@@ -8,10 +8,7 @@ import android.view.KeyEvent
 import com.simplemobiletools.commons.R
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
 import com.simplemobiletools.commons.adapters.FilepickerItemsAdapter
-import com.simplemobiletools.commons.extensions.beVisible
-import com.simplemobiletools.commons.extensions.getFilenameFromPath
-import com.simplemobiletools.commons.extensions.internalStoragePath
-import com.simplemobiletools.commons.extensions.setupDialogStuff
+import com.simplemobiletools.commons.extensions.*
 import com.simplemobiletools.commons.models.FileDirItem
 import com.simplemobiletools.commons.views.Breadcrumbs
 import kotlinx.android.synthetic.main.dialog_filepicker.view.*
