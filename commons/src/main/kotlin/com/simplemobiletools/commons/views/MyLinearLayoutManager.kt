06bfa70fc428fc788cfe45a4b751e18c378febd7
tibbi <tibor@kaputa.sk>
2018-02-04 17:44:05 +0100

    extend Linear and Grid Layout managers, remove predictive item animations

---
+++ commons/src/main/kotlin/com/simplemobiletools/commons/views/MyLinearLayoutManager.kt
@@ -0,0 +1,17 @@
+package com.simplemobiletools.commons.views
+
+import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
+import android.util.AttributeSet
+
+class MyLinearLayoutManager : LinearLayoutManager {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    constructor(context: Context, orientation: Int, reverseLayout: Boolean) : super(context, orientation, reverseLayout)
+
+    // fixes crash java.lang.IndexOutOfBoundsException: Inconsistency detected...
+    // taken from https://stackoverflow.com/a/33985508/1967672
+    override fun supportsPredictiveItemAnimations() = false
+}
