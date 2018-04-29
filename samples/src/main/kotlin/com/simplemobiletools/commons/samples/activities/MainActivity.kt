5fe2b041e6559112e2cba2391aeb1407eeb031f6
tibbi <tibor@kaputa.sk>
2018-04-29 09:41:48 +0200

    add some override helper functions at the sample activity

--- samples/src/main/kotlin/com/simplemobiletools/commons/samples/activities/MainActivity.kt
+++ samples/src/main/kotlin/com/simplemobiletools/commons/samples/activities/MainActivity.kt
@@ -4,8 +4,16 @@ import android.os.Bundle
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
 import com.simplemobiletools.commons.extensions.appLaunched
 import com.simplemobiletools.commons.samples.R
+import java.util.*
 
 class MainActivity : BaseSimpleActivity() {
+    override fun getAppLauncherName() = getString(R.string.smtco_app_name)
+
+    override fun getAppIconIDs(): ArrayList<Int> {
+        val ids = ArrayList<Int>()
+        ids.add(R.mipmap.commons_launcher)
+        return ids
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
