6aafc4d53d01e1ab1566f9353ec4432dec1d92fe
tibbi <tibor@kaputa.sk>
2019-03-07 12:11:37 +0100

    simplify drawable to bitmap converting

--- commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Drawable.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Drawable.kt
@@ -8,21 +8,21 @@ import android.graphics.drawable.Drawable
 
 fun Drawable.applyColorFilter(color: Int) = mutate().setColorFilter(color, PorterDuff.Mode.SRC_IN)
 
-fun Drawable.convertToBitmap(drawable: Drawable): Bitmap {
-    val bitmap = if (drawable.intrinsicWidth <= 0 || drawable.intrinsicHeight <= 0) {
+fun Drawable.convertToBitmap(): Bitmap {
+    val bitmap = if (intrinsicWidth <= 0 || intrinsicHeight <= 0) {
         Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
     } else {
-        Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
     }
 
-    if (drawable is BitmapDrawable) {
-        if (drawable.bitmap != null) {
-            return drawable.bitmap
+    if (this is BitmapDrawable) {
+        if (this.bitmap != null) {
+            return this.bitmap
         }
     }
 
     val canvas = Canvas(bitmap!!)
-    drawable.setBounds(0, 0, canvas.width, canvas.height)
-    drawable.draw(canvas)
+    setBounds(0, 0, canvas.width, canvas.height)
+    draw(canvas)
     return bitmap
 }
