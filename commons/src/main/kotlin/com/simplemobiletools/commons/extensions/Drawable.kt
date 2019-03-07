a15b8fef98fc8ff91c0ae8b96969dbd0a3cdf493
tibbi <tibor@kaputa.sk>
2019-03-07 12:06:20 +0100

    adding a helper drawable extension for converting it to a bitmap

--- commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Drawable.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Drawable.kt
@@ -1,6 +1,28 @@
 package com.simplemobiletools.commons.extensions
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.PorterDuff
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 
 fun Drawable.applyColorFilter(color: Int) = mutate().setColorFilter(color, PorterDuff.Mode.SRC_IN)
+
+fun Drawable.convertToBitmap(drawable: Drawable): Bitmap {
+    val bitmap = if (drawable.intrinsicWidth <= 0 || drawable.intrinsicHeight <= 0) {
+        Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
+    } else {
+        Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+    }
+
+    if (drawable is BitmapDrawable) {
+        if (drawable.bitmap != null) {
+            return drawable.bitmap
+        }
+    }
+
+    val canvas = Canvas(bitmap!!)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+    return bitmap
+}
