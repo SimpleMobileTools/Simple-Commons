1bb8e5f43fdc8daa8d3e9393cab669862d5617bc
tibbi <tibor@kaputa.sk>
2019-05-24 09:49:30 +0200

    creating a dialog for more advanced batch renaming with date formats

--- commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/RenameItemsPatternDialog.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/RenameItemsPatternDialog.kt
@@ -1,18 +1,19 @@
 package com.simplemobiletools.commons.dialogs
 
+import android.media.ExifInterface
 import androidx.appcompat.app.AlertDialog
 import com.simplemobiletools.commons.R
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
 import com.simplemobiletools.commons.extensions.*
-import kotlinx.android.synthetic.main.dialog_rename_items.*
-import kotlinx.android.synthetic.main.dialog_rename_items.view.*
+import com.simplemobiletools.commons.helpers.isNougatPlus
+import kotlinx.android.synthetic.main.dialog_rename_items_pattern.view.*
 import java.io.File
+import java.text.SimpleDateFormat
 import java.util.*
 
-class RenameItemsDialog(val activity: BaseSimpleActivity, val paths: ArrayList<String>, val callback: () -> Unit) {
+class RenameItemsPatternDialog(val activity: BaseSimpleActivity, val paths: ArrayList<String>, val callback: () -> Unit) {
     init {
-
-        val view = activity.layoutInflater.inflate(R.layout.dialog_rename_items, null)
+        val view = activity.layoutInflater.inflate(R.layout.dialog_rename_items_pattern, null)
 
         AlertDialog.Builder(activity)
                 .setPositiveButton(R.string.ok, null)
@@ -21,20 +22,6 @@ class RenameItemsDialog(val activity: BaseSimpleActivity, val paths: ArrayList<S
                     activity.setupDialogStuff(view, this, R.string.rename) {
                         showKeyboard(view.rename_items_value)
                         getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-                            val valueToAdd = view.rename_items_value.value
-                            val append = view.rename_items_radio_group.checkedRadioButtonId == rename_items_radio_append.id
-
-                            if (valueToAdd.isEmpty()) {
-                                callback()
-                                dismiss()
-                                return@setOnClickListener
-                            }
-
-                            if (!valueToAdd.isAValidFilename()) {
-                                activity.toast(R.string.invalid_name)
-                                return@setOnClickListener
-                            }
-
                             val validPaths = paths.filter { File(it).exists() }
                             val sdFilePath = validPaths.firstOrNull { activity.isPathOnSD(it) } ?: validPaths.firstOrNull()
                             if (sdFilePath == null) {
@@ -46,20 +33,32 @@ class RenameItemsDialog(val activity: BaseSimpleActivity, val paths: ArrayList<S
                             activity.handleSAFDialog(sdFilePath) {
                                 var pathsCnt = validPaths.size
                                 for (path in validPaths) {
-                                    val fullName = path.getFilenameFromPath()
-                                    var dotAt = fullName.lastIndexOf(".")
-                                    if (dotAt == -1) {
-                                        dotAt = fullName.length
-                                    }
-
-                                    val name = fullName.substring(0, dotAt)
-                                    val extension = if (fullName.contains(".")) ".${fullName.getFilenameExtension()}" else ""
-
-                                    val newName = if (append) {
-                                        "$name$valueToAdd$extension"
+                                    val exif = ExifInterface(path)
+                                    val dateTime = if (isNougatPlus()) {
+                                        exif.getAttribute(ExifInterface.TAG_DATETIME_ORIGINAL)
+                                                ?: exif.getAttribute(ExifInterface.TAG_DATETIME)
                                     } else {
-                                        "$valueToAdd$fullName"
-                                    }
+                                        exif.getAttribute(ExifInterface.TAG_DATETIME)
+                                    } ?: continue
+
+                                    val simpleDateFormat = SimpleDateFormat("yyyy:MM:dd kk:mm:ss", Locale.ENGLISH)
+                                    val dt = simpleDateFormat.parse(dateTime)
+                                    val cal = Calendar.getInstance()
+                                    cal.time = dt
+                                    val year = ensureTwoDigits(cal.get(Calendar.YEAR))
+                                    val month = ensureTwoDigits(cal.get(Calendar.MONTH) + 1)
+                                    val day = ensureTwoDigits(cal.get(Calendar.DAY_OF_MONTH))
+                                    val hours = ensureTwoDigits(cal.get(Calendar.HOUR_OF_DAY))
+                                    val minutes = ensureTwoDigits(cal.get(Calendar.MINUTE))
+                                    val seconds = ensureTwoDigits(cal.get(Calendar.SECOND))
+
+                                    val newName = view.rename_items_value.value
+                                            .replace("%Y", year, false)
+                                            .replace("%M", month, false)
+                                            .replace("%D", day, false)
+                                            .replace("%h", hours, false)
+                                            .replace("%m", minutes, false)
+                                            .replace("%s", seconds, false)
 
                                     val newPath = "${path.getParentPath()}/$newName"
 
@@ -84,5 +83,13 @@ class RenameItemsDialog(val activity: BaseSimpleActivity, val paths: ArrayList<S
                         }
                     }
                 }
+    }
+
+    private fun ensureTwoDigits(value: Int): String {
+        return if (value.toString().length == 1) {
+            "0$value"
+        } else {
+            value.toString()
+        }
     }
 }
