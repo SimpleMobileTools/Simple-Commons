77f12c130a4cad7a39ef5ced96e33ce248018850
tibbi <tibor@kaputa.sk>
2019-07-08 23:29:05 +0200

    add support for heic and heif files

--- commons/src/main/kotlin/com/simplemobiletools/commons/helpers/Constants.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/helpers/Constants.kt
@@ -209,7 +209,7 @@ const val SIDELOADING_UNCHECKED = 0
 const val SIDELOADING_TRUE = 1
 const val SIDELOADING_FALSE = 2
 
-val photoExtensions: Array<String> get() = arrayOf(".jpg", ".png", ".jpeg", ".bmp", ".webp")
+val photoExtensions: Array<String> get() = arrayOf(".jpg", ".png", ".jpeg", ".bmp", ".webp", ".heic", ".heif")
 val videoExtensions: Array<String> get() = arrayOf(".mp4", ".mkv", ".webm", ".avi", ".3gp", ".mov", ".m4v", ".3gpp")
 val audioExtensions: Array<String> get() = arrayOf(".mp3", ".wav", ".wma", ".ogg", ".m4a", ".opus", ".flac", ".aac")
 val rawExtensions: Array<String> get() = arrayOf(".dng", ".orf", ".nef", ".arw", ".cr2")
