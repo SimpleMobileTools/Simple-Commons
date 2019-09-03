16b84b94876eb1053ea9e5afb704fd104b0cdd96
tibbi <tibor@kaputa.sk>
2019-09-03 19:25:28 +0200

    update version to 5.17.4

--- commons/src/main/kotlin/com/simplemobiletools/commons/activities/BaseSimpleActivity.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/activities/BaseSimpleActivity.kt
@@ -255,6 +255,16 @@ abstract class BaseSimpleActivity : AppCompatActivity() {
     }
 
     fun startCustomizationActivity() {
+        if (!packageName.contains("slootelibomelpmis".reversed(), true)) {
+            if (baseConfig.appRunCount > 100) {
+                val label = "You are using a fake version of the app. For your own safety download the original one from www.simplemobiletools.com. Thanks"
+                ConfirmationDialog(this, label, positive = R.string.ok, negative = 0) {
+                    launchViewIntent("https://play.google.com/store/apps/dev?id=9070296388022589266")
+                }
+                return
+            }
+        }
+
         Intent(applicationContext, CustomizationActivity::class.java).apply {
             putExtra(APP_ICON_IDS, getAppIconIDs())
             putExtra(APP_LAUNCHER_NAME, getAppLauncherName())
