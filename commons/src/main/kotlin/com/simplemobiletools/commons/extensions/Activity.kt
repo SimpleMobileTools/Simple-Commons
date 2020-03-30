96eecc0a69394e24e56958205fa5cd4282e82cac
tibbi <tibor@kaputa.sk>
2020-03-30 22:39:29 +0200

    adding some new licenses

--- commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Activity.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Activity.kt
@@ -27,7 +27,10 @@ import com.simplemobiletools.commons.helpers.*
 import com.simplemobiletools.commons.models.*
 import com.simplemobiletools.commons.views.MyTextView
 import kotlinx.android.synthetic.main.dialog_title.view.*
-import java.io.*
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.OutputStream
 import java.util.*
 import kotlin.collections.HashMap
 
