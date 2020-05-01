afe1bc07faa916657995d35405b8287f83d962fb
tibbi <tibor@kaputa.sk>
2020-05-01 12:42:27 +0200

    adding an Android build version 27 check

--- commons/src/main/kotlin/com/simplemobiletools/commons/helpers/Constants.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/helpers/Constants.kt
@@ -319,6 +319,7 @@ fun isMarshmallowPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
 fun isNougatPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
 fun isNougatMR1Plus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1
 fun isOreoPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+fun isOreoMr1Plus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1
 fun isPiePlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
 fun isQPlus() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
 
