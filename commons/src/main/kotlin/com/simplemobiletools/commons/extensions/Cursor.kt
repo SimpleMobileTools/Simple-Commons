bef619280b4c630499ed759ca9c730b3a6bf981e
tibbi <tibor@kaputa.sk>
2021-09-20 22:25:29 +0200

    adding a new getStringValueOrNull extension

--- commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Cursor.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/extensions/Cursor.kt
@@ -4,6 +4,8 @@ import android.database.Cursor
 
 fun Cursor.getStringValue(key: String) = getString(getColumnIndex(key))
 
+fun Cursor.getStringValueOrNull(key: String) = if (isNull(getColumnIndex(key))) null else getString(getColumnIndex(key))
+
 fun Cursor.getIntValue(key: String) = getInt(getColumnIndex(key))
 
 fun Cursor.getIntValueOrNull(key: String) = if (isNull(getColumnIndex(key))) null else getInt(getColumnIndex(key))
