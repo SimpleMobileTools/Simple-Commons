907a3999ce775b79996966f6eea38455acce1588
tibbi <tibor@kaputa.sk>
2021-10-24 23:22:40 +0200

    list all languages at the contributors

--- commons/src/main/kotlin/com/simplemobiletools/commons/activities/ContributorsActivity.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/activities/ContributorsActivity.kt
@@ -2,8 +2,12 @@ package com.simplemobiletools.commons.activities
 
 import android.os.Bundle
 import com.simplemobiletools.commons.R
+import com.simplemobiletools.commons.extensions.getAdjustedPrimaryColor
+import com.simplemobiletools.commons.extensions.underlineText
+import com.simplemobiletools.commons.extensions.updateTextColors
 import com.simplemobiletools.commons.helpers.APP_ICON_IDS
 import com.simplemobiletools.commons.helpers.APP_LAUNCHER_NAME
+import kotlinx.android.synthetic.main.activity_contributors.*
 import java.util.*
 
 class ContributorsActivity : BaseSimpleActivity() {
@@ -14,5 +18,13 @@ class ContributorsActivity : BaseSimpleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contributors)
+
+        updateTextColors(contributors_holder)
+
+        contributors_development_label.setTextColor(getAdjustedPrimaryColor())
+        contributors_development_label.underlineText()
+
+        contributors_translation_label.setTextColor(getAdjustedPrimaryColor())
+        contributors_translation_label.underlineText()
     }
 }
