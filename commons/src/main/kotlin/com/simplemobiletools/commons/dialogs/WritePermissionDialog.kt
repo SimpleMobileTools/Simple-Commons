a55a0cb15d56b02bfdaac0d2ed1fc3315caa828d
tibbi <tibor@kaputa.sk>
2022-03-13 18:23:07 +0100

    treat clicking on the SDK 30 dialog image as OK

--- commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/WritePermissionDialog.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/WritePermissionDialog.kt
@@ -37,6 +37,10 @@ class WritePermissionDialog(activity: Activity, val mode: Mode, val callback: ()
             Mode.SDK_30 -> {
                 view.write_permissions_dialog_otg_text.setText(R.string.confirm_storage_access_android_text)
                 glide.load(R.drawable.img_write_storage_sdk_30).transition(crossFade).into(view.write_permissions_dialog_otg_image)
+
+                view.write_permissions_dialog_otg_image.setOnClickListener {
+                    dialogConfirmed()
+                }
             }
         }
 
