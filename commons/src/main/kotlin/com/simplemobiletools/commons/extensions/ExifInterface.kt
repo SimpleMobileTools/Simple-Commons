0956159a766f1afe7166ddc3eb74651c5aa0b546
tibbi <tibor@kaputa.sk>
2022-03-20 18:40:43 +0100

    adding a helper function for removing EXIF values of a path

--- commons/src/main/kotlin/com/simplemobiletools/commons/extensions/ExifInterface.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/extensions/ExifInterface.kt
@@ -49,7 +49,43 @@ fun ExifInterface.copyTo(destination: ExifInterface, copyOrientation: Boolean = 
     }
 }
 
-@TargetApi(Build.VERSION_CODES.N)
+fun ExifInterface.removeValues(path: String) {
+    val newExif = ExifInterface(path)
+
+    val attributes = arrayListOf(
+        // ExifInterface.TAG_ORIENTATION,   // do not remove the orientation, it could lead to unexpected behaviour at displaying the file
+        ExifInterface.TAG_APERTURE_VALUE,
+        ExifInterface.TAG_DATETIME,
+        ExifInterface.TAG_DATETIME_DIGITIZED,
+        ExifInterface.TAG_DATETIME_ORIGINAL,
+        ExifInterface.TAG_EXPOSURE_TIME,
+        ExifInterface.TAG_FLASH,
+        ExifInterface.TAG_F_NUMBER,
+        ExifInterface.TAG_FOCAL_LENGTH,
+        ExifInterface.TAG_GPS_ALTITUDE,
+        ExifInterface.TAG_GPS_ALTITUDE_REF,
+        ExifInterface.TAG_GPS_DATESTAMP,
+        ExifInterface.TAG_GPS_LATITUDE,
+        ExifInterface.TAG_GPS_LATITUDE_REF,
+        ExifInterface.TAG_GPS_LONGITUDE,
+        ExifInterface.TAG_GPS_LONGITUDE_REF,
+        ExifInterface.TAG_GPS_PROCESSING_METHOD,
+        ExifInterface.TAG_GPS_TIMESTAMP,
+        ExifInterface.TAG_IMAGE_LENGTH,
+        ExifInterface.TAG_IMAGE_WIDTH,
+        ExifInterface.TAG_ISO_SPEED_RATINGS,
+        ExifInterface.TAG_MAKE,
+        ExifInterface.TAG_MODEL,
+        ExifInterface.TAG_WHITE_BALANCE
+    )
+
+    attributes.forEach {
+        newExif.setAttribute(it, null)
+    }
+
+    newExif.saveAttributes()
+}
+
 fun ExifInterface.getExifProperties(): String {
     var exifString = ""
     getAttribute(ExifInterface.TAG_F_NUMBER).let {
