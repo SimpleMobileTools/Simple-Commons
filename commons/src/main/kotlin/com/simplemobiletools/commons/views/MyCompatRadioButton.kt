8b49442b9f5520a8b86964ba904947b9d27fa6ab
tibbi <tibor@kaputa.sk>
2022-03-21 19:01:34 +0100

    use the text color at inactive radiobuttons

--- commons/src/main/kotlin/com/simplemobiletools/commons/views/MyCompatRadioButton.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/views/MyCompatRadioButton.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatRadioButton
-import com.simplemobiletools.commons.R
 
 class MyCompatRadioButton : AppCompatRadioButton {
     constructor(context: Context) : super(context)
@@ -16,10 +15,11 @@ class MyCompatRadioButton : AppCompatRadioButton {
     fun setColors(textColor: Int, accentColor: Int, backgroundColor: Int) {
         setTextColor(textColor)
         val colorStateList = ColorStateList(
-                arrayOf(intArrayOf(-android.R.attr.state_checked),
-                        intArrayOf(android.R.attr.state_checked)
-                ),
-                intArrayOf(context.resources.getColor(R.color.radiobutton_disabled), accentColor)
+            arrayOf(
+                intArrayOf(-android.R.attr.state_checked),
+                intArrayOf(android.R.attr.state_checked)
+            ),
+            intArrayOf(textColor, accentColor)
         )
         supportButtonTintList = colorStateList
     }
