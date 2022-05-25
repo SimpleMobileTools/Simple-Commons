14916e65898d89e8035726390ea093ead4d8f7c0
Pavel Poley <pavelpoley88@gmail.com>
2022-05-25 12:29:54 +0300

    make isPrimary mutable

--- commons/src/main/kotlin/com/simplemobiletools/commons/models/PhoneNumber.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/models/PhoneNumber.kt
@@ -1,4 +1,4 @@
 package com.simplemobiletools.commons.models
 
-data class PhoneNumber(var value: String, var type: Int, var label: String, var normalizedNumber: String, val isPrimary: Boolean = false)
+data class PhoneNumber(var value: String, var type: Int, var label: String, var normalizedNumber: String, var isPrimary: Boolean = false)
 
