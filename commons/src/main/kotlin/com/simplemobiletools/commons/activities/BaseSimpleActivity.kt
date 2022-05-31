0761c710e4393a79b32d633290f2b7d641452530
tibbi <tibor@kaputa.sk>
2022-05-31 15:24:42 +0200

    fixing some spacing

--- commons/src/main/kotlin/com/simplemobiletools/commons/activities/BaseSimpleActivity.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/activities/BaseSimpleActivity.kt
@@ -660,7 +660,7 @@ abstract class BaseSimpleActivity : AppCompatActivity() {
                 copyMoveCallback = callback
                 var fileCountToCopy = fileDirItems.size
                 if (isCopyOperation) {
-                    if(canManageMedia()){
+                    if (canManageMedia()) {
                         val fileUris = getFileUrisFromFileDirItems(fileDirItems).second
                         updateSDK30Uris(fileUris) { sdk30UriSuccess ->
                             if (sdk30UriSuccess) {
@@ -678,7 +678,7 @@ abstract class BaseSimpleActivity : AppCompatActivity() {
                     ) {
                         handleSAFDialog(source) { safSuccess ->
                             if (safSuccess) {
-                                if(canManageMedia()){
+                                if (canManageMedia()) {
                                     val fileUris = getFileUrisFromFileDirItems(fileDirItems).second
                                     updateSDK30Uris(fileUris) { sdk30UriSuccess ->
                                         if (sdk30UriSuccess) {
