3c2ffc8c42607c84cec3e4e89e78b27da29367e8
tibbi <tibor@kaputa.sk>
2022-06-06 15:52:02 +0200

    use different dialog titles at Use this folder dialog

--- commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/WritePermissionDialog.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/WritePermissionDialog.kt
@@ -25,6 +25,7 @@ class WritePermissionDialog(activity: Activity, val mode: Mode, val callback: ()
     init {
         val layout = if (mode == Mode.SdCard) R.layout.dialog_write_permission else R.layout.dialog_write_permission_otg
         val view = activity.layoutInflater.inflate(layout, null)
+        var dialogTitle = R.string.confirm_storage_access_title
 
         val glide = Glide.with(activity)
         val crossFade = DrawableTransitionOptions.withCrossFade()
@@ -38,6 +39,7 @@ class WritePermissionDialog(activity: Activity, val mode: Mode, val callback: ()
                 glide.load(R.drawable.img_write_storage_sd).transition(crossFade).into(view.write_permissions_dialog_image_sd)
             }
             is Mode.OpenDocumentTreeSDK30 -> {
+                dialogTitle = R.string.confirm_folder_access_title
                 val humanizedPath = activity.humanizePath(mode.path)
                 view.write_permissions_dialog_otg_text.text =
                     Html.fromHtml(activity.getString(R.string.confirm_storage_access_android_text_specific, humanizedPath))
@@ -48,6 +50,7 @@ class WritePermissionDialog(activity: Activity, val mode: Mode, val callback: ()
                 }
             }
             Mode.CreateDocumentSDK30 -> {
+                dialogTitle = R.string.confirm_folder_access_title
                 view.write_permissions_dialog_otg_text.text = Html.fromHtml(activity.getString(R.string.confirm_create_doc_for_new_folder_text))
                 glide.load(R.drawable.img_write_storage_create_doc_sdk_30).transition(crossFade).into(view.write_permissions_dialog_otg_image)
 
@@ -64,7 +67,7 @@ class WritePermissionDialog(activity: Activity, val mode: Mode, val callback: ()
                 BaseSimpleActivity.funAfterSAFPermission = null
             }
             .create().apply {
-                activity.setupDialogStuff(view, this, R.string.confirm_storage_access_title)
+                activity.setupDialogStuff(view, this, dialogTitle)
             }
     }
 
