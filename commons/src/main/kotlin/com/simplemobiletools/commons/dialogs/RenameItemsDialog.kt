c052e8cbe8839cc648487677368d7341f0a1b7bb
tibbi <tibor@kaputa.sk>
2022-06-29 10:30:11 +0200

    adding a free alternative of a string

--- commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/RenameItemsDialog.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/RenameItemsDialog.kt
@@ -6,7 +6,6 @@ import com.simplemobiletools.commons.activities.BaseSimpleActivity
 import com.simplemobiletools.commons.extensions.*
 import kotlinx.android.synthetic.main.dialog_rename_items.*
 import kotlinx.android.synthetic.main.dialog_rename_items.view.*
-import java.util.*
 
 // used at renaming folders
 class RenameItemsDialog(val activity: BaseSimpleActivity, val paths: ArrayList<String>, val callback: () -> Unit) {
