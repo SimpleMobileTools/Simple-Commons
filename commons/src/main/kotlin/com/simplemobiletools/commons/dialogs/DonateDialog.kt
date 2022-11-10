299657f0757c8c56774b39bcc70521a59402852d
tibbi <tibor@kaputa.sk>
2022-11-10 23:13:05 +0100

    tweak donate dialog a bit

--- commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/DonateDialog.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/dialogs/DonateDialog.kt
@@ -4,16 +4,18 @@ import android.app.Activity
 import android.text.Html
 import android.text.method.LinkMovementMethod
 import com.simplemobiletools.commons.R
-import com.simplemobiletools.commons.extensions.getAlertDialogBuilder
-import com.simplemobiletools.commons.extensions.launchViewIntent
-import com.simplemobiletools.commons.extensions.setupDialogStuff
-import kotlinx.android.synthetic.main.dialog_textview.view.*
+import com.simplemobiletools.commons.extensions.*
+import kotlinx.android.synthetic.main.dialog_donate.view.*
 
 class DonateDialog(val activity: Activity) {
     init {
-        val view = activity.layoutInflater.inflate(R.layout.dialog_textview, null).apply {
-            text_view.text = Html.fromHtml(activity.getString(R.string.donate_please))
-            text_view.movementMethod = LinkMovementMethod.getInstance()
+        val view = activity.layoutInflater.inflate(R.layout.dialog_donate, null).apply {
+            dialog_donate_image.applyColorFilter(activity.getProperTextColor())
+            dialog_donate_text.text = Html.fromHtml(activity.getString(R.string.donate_short))
+            dialog_donate_text.movementMethod = LinkMovementMethod.getInstance()
+            dialog_donate_image.setOnClickListener {
+                activity.launchViewIntent(R.string.thank_you_url)
+            }
         }
 
         activity.getAlertDialogBuilder()
