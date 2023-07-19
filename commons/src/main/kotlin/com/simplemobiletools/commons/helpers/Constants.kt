fb28c2702ddc79dd72f272b2570cc3093f8512ed
Naveen <snaveen935@gmail.com>
2023-07-19 22:29:06 +0530

    Add 'm4b' to the list of audio extensions

--- commons/src/main/kotlin/com/simplemobiletools/commons/helpers/Constants.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/helpers/Constants.kt
@@ -363,7 +363,7 @@ const val TAB_STORAGE_ANALYSIS = 64
 
 val photoExtensions: Array<String> get() = arrayOf(".jpg", ".png", ".jpeg", ".bmp", ".webp", ".heic", ".heif", ".apng", ".avif")
 val videoExtensions: Array<String> get() = arrayOf(".mp4", ".mkv", ".webm", ".avi", ".3gp", ".mov", ".m4v", ".3gpp")
-val audioExtensions: Array<String> get() = arrayOf(".mp3", ".wav", ".wma", ".ogg", ".m4a", ".opus", ".flac", ".aac")
+val audioExtensions: Array<String> get() = arrayOf(".mp3", ".wav", ".wma", ".ogg", ".m4a", ".opus", ".flac", ".aac", "m4b")
 val rawExtensions: Array<String> get() = arrayOf(".dng", ".orf", ".nef", ".arw", ".rw2", ".cr2", ".cr3")
 
 val extensionsSupportingEXIF: Array<String> get() = arrayOf(".jpg", ".jpeg", ".png", ".webp", ".dng")
