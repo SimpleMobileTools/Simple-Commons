f8ad23bd26c18c9a8cfa136ef3947dc17dc8af98
FunkyMuse <kikoski@protonmail.com>
2023-08-03 12:23:43 +0200

    fix: ui cosmetics

--- commons/src/main/kotlin/com/simplemobiletools/commons/compose/screens/AboutScreen.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/compose/screens/AboutScreen.kt
@@ -13,7 +13,7 @@ import com.simplemobiletools.commons.compose.extensions.MyDevices
 import com.simplemobiletools.commons.compose.settings.*
 import com.simplemobiletools.commons.compose.theme.AppThemeSurface
 
-private val startingTitlePadding = Modifier.padding(horizontal = 4.dp)
+private val startingTitlePadding = Modifier.padding(start = 64.dp)
 
 @Composable
 internal fun AboutScreen(
@@ -45,7 +45,9 @@ private fun MadeWithLove() {
         },
         leadingContent = {
             Box(
-                modifier = Modifier.size(24.dp),
+                modifier = Modifier
+                    .size(48.dp)
+                    .padding(8.dp),
             )
         }
     )
@@ -65,14 +67,34 @@ internal fun HelpUsSection(
         SettingsTitleTextComponent(text = stringResource(id = R.string.help_us), modifier = startingTitlePadding)
     }) {
         if (showRateUs) {
-            SettingsListItem(click = onRateUsClick, text = stringResource(id = R.string.rate_us), icon = R.drawable.ic_star_vector)
+            SettingsListItem(
+                tint = MaterialTheme.colorScheme.onSurface,
+                click = onRateUsClick,
+                text = stringResource(id = R.string.rate_us),
+                icon = R.drawable.ic_star_vector
+            )
         }
         if (showInvite) {
-            SettingsListItem(click = onInviteClick, text = stringResource(id = R.string.invite_friends), icon = R.drawable.ic_add_person_vector)
+            SettingsListItem(
+                tint = MaterialTheme.colorScheme.onSurface,
+                click = onInviteClick,
+                text = stringResource(id = R.string.invite_friends),
+                icon = R.drawable.ic_add_person_vector
+            )
         }
-        SettingsListItem(click = onContributorsClick, text = stringResource(id = R.string.contributors), icon = R.drawable.ic_face_vector)
+        SettingsListItem(
+            tint = MaterialTheme.colorScheme.onSurface,
+            click = onContributorsClick,
+            text = stringResource(id = R.string.contributors),
+            icon = R.drawable.ic_face_vector
+        )
         if (showDonate) {
-            SettingsListItem(click = onDonateClick, text = stringResource(id = R.string.donate), icon = R.drawable.ic_dollar_vector)
+            SettingsListItem(
+                tint = MaterialTheme.colorScheme.onSurface,
+                click = onDonateClick,
+                text = stringResource(id = R.string.donate),
+                icon = R.drawable.ic_dollar_vector
+            )
         }
         SettingsHorizontalDivider()
     }
@@ -94,16 +116,36 @@ internal fun OtherSection(
         SettingsTitleTextComponent(text = stringResource(id = R.string.other), modifier = startingTitlePadding)
     }) {
         if (showMoreApps) {
-            SettingsListItem(click = onMoreAppsClick, text = stringResource(id = R.string.more_apps_from_us), icon = R.drawable.ic_heart_vector)
+            SettingsListItem(
+                tint = MaterialTheme.colorScheme.onSurface,
+                click = onMoreAppsClick,
+                text = stringResource(id = R.string.more_apps_from_us),
+                icon = R.drawable.ic_heart_vector
+            )
         }
         if (showWebsite) {
-            SettingsListItem(click = onWebsiteClick, text = stringResource(id = R.string.website), icon = R.drawable.ic_link_vector)
+            SettingsListItem(
+                tint = MaterialTheme.colorScheme.onSurface,
+                click = onWebsiteClick,
+                text = stringResource(id = R.string.website),
+                icon = R.drawable.ic_link_vector
+            )
         }
         if (showPrivacyPolicy) {
-            SettingsListItem(click = onPrivacyPolicyClick, text = stringResource(id = R.string.privacy_policy), icon = R.drawable.ic_unhide_vector)
+            SettingsListItem(
+                tint = MaterialTheme.colorScheme.onSurface,
+                click = onPrivacyPolicyClick,
+                text = stringResource(id = R.string.privacy_policy),
+                icon = R.drawable.ic_unhide_vector
+            )
         }
-        SettingsListItem(click = onLicenseClick, text = stringResource(id = R.string.third_party_licences), icon = R.drawable.ic_article_vector)
-        SettingsListItem(click = onVersionClick, text = version, icon = R.drawable.ic_info_vector)
+        SettingsListItem(
+            tint = MaterialTheme.colorScheme.onSurface,
+            click = onLicenseClick,
+            text = stringResource(id = R.string.third_party_licences),
+            icon = R.drawable.ic_article_vector
+        )
+        SettingsListItem(tint = MaterialTheme.colorScheme.onSurface, click = onVersionClick, text = version, icon = R.drawable.ic_info_vector)
         SettingsHorizontalDivider()
     }
 }
@@ -119,9 +161,19 @@ internal fun AboutSection(
         SettingsTitleTextComponent(text = stringResource(id = R.string.support), modifier = startingTitlePadding)
     }) {
         if (setupFAQ) {
-            SettingsListItem(click = onFAQClick, text = stringResource(id = R.string.frequently_asked_questions), icon = R.drawable.ic_question_mark_vector)
+            SettingsListItem(
+                tint = MaterialTheme.colorScheme.onSurface,
+                click = onFAQClick,
+                text = stringResource(id = R.string.frequently_asked_questions),
+                icon = R.drawable.ic_question_mark_vector
+            )
         }
-        SettingsListItem(click = onEmailClick, text = stringResource(id = R.string.my_email), icon = R.drawable.ic_mail_vector)
+        SettingsListItem(
+            tint = MaterialTheme.colorScheme.onSurface,
+            click = onEmailClick,
+            text = stringResource(id = R.string.my_email),
+            icon = R.drawable.ic_mail_vector
+        )
         SettingsHorizontalDivider()
     }
 }
@@ -136,10 +188,31 @@ internal fun SocialSection(
     SettingsGroup(title = {
         SettingsTitleTextComponent(text = stringResource(id = R.string.social), modifier = startingTitlePadding)
     }) {
-        SettingsListItem(click = onFacebookClick, text = stringResource(id = R.string.facebook), icon = R.drawable.ic_facebook_vector, isImage = true)
-        SettingsListItem(click = onGithubClick, text = stringResource(id = R.string.github), icon = R.drawable.ic_github_vector, isImage = true, tint = MaterialTheme.colorScheme.onSurface)
-        SettingsListItem(click = onRedditClick, text = stringResource(id = R.string.reddit), icon = R.drawable.ic_reddit_vector, isImage = true)
-        SettingsListItem(click = onTelegramClick, text = stringResource(id = R.string.telegram), icon = R.drawable.ic_telegram_vector, isImage = true)
+        SettingsListItem(
+            click = onFacebookClick,
+            text = stringResource(id = R.string.facebook),
+            icon = R.drawable.ic_facebook_vector,
+            isImage = true
+        )
+        SettingsListItem(
+            click = onGithubClick,
+            text = stringResource(id = R.string.github),
+            icon = R.drawable.ic_github_vector,
+            isImage = true,
+            tint = MaterialTheme.colorScheme.onSurface
+        )
+        SettingsListItem(
+            click = onRedditClick,
+            text = stringResource(id = R.string.reddit),
+            icon = R.drawable.ic_reddit_vector,
+            isImage = true
+        )
+        SettingsListItem(
+            click = onTelegramClick,
+            text = stringResource(id = R.string.telegram),
+            icon = R.drawable.ic_telegram_vector,
+            isImage = true
+        )
         SettingsHorizontalDivider()
     }
 }
