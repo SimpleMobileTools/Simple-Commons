b2c561b2effe053aee825c683b59b4355562d14c
FunkyMuse <kikoski@protonmail.com>
2023-08-03 13:17:42 +0200

    refactor: add color as a parameter

--- commons/src/main/kotlin/com/simplemobiletools/commons/compose/settings/SettingsTitleTextComponent.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/compose/settings/SettingsTitleTextComponent.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
@@ -16,12 +17,13 @@ import com.simplemobiletools.commons.compose.theme.AppThemeSurface
 fun SettingsTitleTextComponent(
     modifier: Modifier = Modifier,
     text: String,
+    color: Color = MaterialTheme.colorScheme.primary,
 ) {
     Box(modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)) {
         Text(
             text = text.uppercase(),
             modifier = modifier,
-            color = MaterialTheme.colorScheme.primary,
+            color = color,
             fontSize = with(LocalDensity.current) {
                 dimensionResource(id = com.simplemobiletools.commons.R.dimen.normal_text_size).toSp()
             },
