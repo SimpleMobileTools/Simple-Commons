c6938771bd9e6610bab96570c8f4b3a805520aea
FunkyMuse <kikoski@protonmail.com>
2023-08-08 22:38:13 +0200

    feat: migrate license to compose

--- commons/src/main/kotlin/com/simplemobiletools/commons/models/License.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/models/License.kt
@@ -1,3 +1,6 @@
 package com.simplemobiletools.commons.models
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class License(val id: Long, val titleId: Int, val textId: Int, val urlId: Int)
