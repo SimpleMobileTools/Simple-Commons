f057712508bba05b137374af2fbf0431aa578ac7
FunkyMuse <kikoski@protonmail.com>
2023-08-28 14:15:39 +0200

    refactor: remove unneeded code

--- commons/src/main/kotlin/com/simplemobiletools/commons/activities/FAQActivity.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/activities/FAQActivity.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import com.simplemobiletools.commons.compose.extensions.AdjustNavigationBarColors
 import com.simplemobiletools.commons.compose.screens.FAQScreen
 import com.simplemobiletools.commons.compose.theme.AppThemeSurface
 import com.simplemobiletools.commons.helpers.APP_FAQ
@@ -21,8 +17,6 @@ class FAQActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppThemeSurface {
-                var canScroll by remember { mutableStateOf<Boolean?>(null) }
-                AdjustNavigationBarColors(canScroll)
                 val faqItems = remember { intent.getSerializableExtra(APP_FAQ) as ArrayList<FAQItem> }
                 FAQScreen(
                     goBack = ::finish,
