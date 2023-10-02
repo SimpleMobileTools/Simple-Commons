1df399e0af300ed18c0a7b40ef2f9fd9a54e38b8
FunkyMuse <kikoski@protonmail.com>
2023-10-02 11:23:19 +0200

    feat: add infix and inline chaining functions

--- commons/src/main/kotlin/com/simplemobiletools/commons/compose/extensions/ModifierExtensions.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/compose/extensions/ModifierExtensions.kt
@@ -8,3 +8,27 @@ inline fun Modifier.ifTrue(predicate: Boolean, builder: () -> Modifier) =
 inline fun Modifier.ifFalse(predicate: Boolean, builder: () -> Modifier) =
     then(if (!predicate) builder() else Modifier)
 
+inline infix fun (() -> Unit).andThen(crossinline function: () -> Unit): () -> Unit = {
+    this()
+    function()
+}
+
+inline fun (() -> Unit).andThen(
+    crossinline function: () -> Unit,
+    crossinline function2: () -> Unit
+): () -> Unit = {
+    this()
+    function()
+    function2()
+}
+
+inline fun (() -> Unit).andThen(
+    crossinline function: () -> Unit,
+    crossinline function2: () -> Unit,
+    crossinline function3: () -> Unit,
+): () -> Unit = {
+    this()
+    function()
+    function2()
+    function3()
+}
