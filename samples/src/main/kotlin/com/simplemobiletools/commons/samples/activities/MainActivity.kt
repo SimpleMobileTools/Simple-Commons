e587a90faf2375e35dcb1ae2ef7701a1c4ce76fb
Ensar Sarajčić <dev@ensarsarajcic.com>
2023-10-06 13:25:59 +0200

    Migrate sample main activity to compose for easier testing

--- samples/src/main/kotlin/com/simplemobiletools/commons/samples/activities/MainActivity.kt
+++ samples/src/main/kotlin/com/simplemobiletools/commons/samples/activities/MainActivity.kt
@@ -2,58 +2,180 @@ package com.simplemobiletools.commons.samples.activities
 
 import android.content.Intent
 import android.os.Bundle
-import com.simplemobiletools.commons.activities.BaseSimpleActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.simplemobiletools.commons.activities.AboutActivity
+import com.simplemobiletools.commons.activities.CustomizationActivity
 import com.simplemobiletools.commons.activities.ManageBlockedNumbersActivity
+import com.simplemobiletools.commons.compose.alert_dialog.AlertDialogState
+import com.simplemobiletools.commons.compose.alert_dialog.rememberAlertDialogState
 import com.simplemobiletools.commons.compose.extensions.DEVELOPER_PLAY_STORE_URL
 import com.simplemobiletools.commons.compose.extensions.FAKE_VERSION_APP_LABEL
-import com.simplemobiletools.commons.dialogs.BottomSheetChooserDialog
+import com.simplemobiletools.commons.compose.extensions.appLaunchedCompose
+import com.simplemobiletools.commons.compose.extensions.rateStarsRedirectAndThankYou
+import com.simplemobiletools.commons.compose.menus.ActionItem
+import com.simplemobiletools.commons.compose.menus.ActionMenu
+import com.simplemobiletools.commons.compose.menus.OverflowMode
+import com.simplemobiletools.commons.compose.settings.scaffold.SettingsLazyScaffold
+import com.simplemobiletools.commons.compose.settings.scaffold.topAppBarColors
+import com.simplemobiletools.commons.compose.settings.scaffold.topAppBarInsets
+import com.simplemobiletools.commons.compose.settings.scaffold.topAppBarPaddings
+import com.simplemobiletools.commons.compose.theme.AppThemeSurface
 import com.simplemobiletools.commons.dialogs.ConfirmationDialog
-import com.simplemobiletools.commons.extensions.appLaunched
+import com.simplemobiletools.commons.dialogs.DonateAlertDialog
+import com.simplemobiletools.commons.dialogs.RateStarsAlertDialog
+import com.simplemobiletools.commons.extensions.hideKeyboard
+import com.simplemobiletools.commons.extensions.launchMoreAppsFromUsIntent
 import com.simplemobiletools.commons.extensions.launchViewIntent
-import com.simplemobiletools.commons.extensions.toast
-import com.simplemobiletools.commons.extensions.viewBinding
-import com.simplemobiletools.commons.helpers.LICENSE_AUTOFITTEXTVIEW
+import com.simplemobiletools.commons.helpers.*
 import com.simplemobiletools.commons.models.FAQItem
-import com.simplemobiletools.commons.models.SimpleListItem
 import com.simplemobiletools.commons.samples.BuildConfig
 import com.simplemobiletools.commons.samples.R
-import com.simplemobiletools.commons.samples.databinding.ActivityMainBinding
+import kotlinx.collections.immutable.toImmutableList
 
-class MainActivity : BaseSimpleActivity() {
-    override fun getAppLauncherName() = getString(R.string.smtco_app_name)
-
-    override fun getAppIconIDs(): ArrayList<Int> {
-        val ids = ArrayList<Int>()
-        ids.add(R.mipmap.commons_launcher)
-        return ids
-    }
-
-    private val binding by viewBinding(ActivityMainBinding::inflate)
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        appLaunched(BuildConfig.APPLICATION_ID)
 
-        updateMaterialActivityViews(binding.mainCoordinator, binding.mainHolder, useTransparentNavigation = true, useTopSearchMenu = false)
-        setupMaterialScrollListener(binding.mainNestedScrollview, binding.mainToolbar)
+        setContent {
+            AppThemeSurface {
+                SettingsLazyScaffold(
+                    customTopBar = { scrolledColor: Color, _: MutableInteractionSource, scrollBehavior: TopAppBarScrollBehavior, statusBarColor: Int, colorTransitionFraction: Float, contrastColor: Color ->
+                        TopAppBar(
+                            title = {},
+                            actions = {
+                                val actionMenus = remember {
+                                    val about = ActionItem(
+                                        R.string.about,
+                                        icon = Icons.Outlined.Info,
+                                        doAction = ::launchAbout,
+                                        overflowMode = OverflowMode.NEVER_OVERFLOW
+                                    )
+                                    val moreApps =
+                                        ActionItem(
+                                            R.string.more_apps_from_us,
+                                            doAction = ::launchMoreAppsFromUsIntent,
+                                            overflowMode = OverflowMode.ALWAYS_OVERFLOW
+                                        )
+                                    listOf(about, moreApps).toImmutableList()
+                                }
+                                var isMenuVisible by remember { mutableStateOf(false) }
+                                ActionMenu(
+                                    items = actionMenus,
+                                    numIcons = 2,
+                                    isMenuVisible = isMenuVisible,
+                                    onMenuToggle = { isMenuVisible = it },
+                                    iconsColor = scrolledColor
+                                )
+                            },
+                            scrollBehavior = scrollBehavior,
+                            colors = topAppBarColors(statusBarColor, colorTransitionFraction, contrastColor),
+                            modifier = Modifier.topAppBarPaddings(),
+                            windowInsets = topAppBarInsets()
+                        )
+                    }
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(
+                            onClick = ::startCustomizationActivity
+                        ) {
+                            Text(stringResource(id = R.string.color_customization))
+                        }
+                        Button(
+                            onClick = ::launchAbout
+                        ) {
+                            Text("About")
+                        }
+                        Button(
+                            onClick = {
+                                startActivity(Intent(this@MainActivity, ManageBlockedNumbersActivity::class.java))
+                            }
+                        ) {
+                            Text("Manage blocked numbers")
+                        }
+                        Button(
+                            onClick = {
+                                startActivity(Intent(this@MainActivity, TestDialogActivity::class.java))
+                            }
+                        ) {
+                            Text("Compose dialogs")
+                        }
+                        Button(
+                            onClick = {
+                                ConfirmationDialog(
+                                    this@MainActivity,
+                                    FAKE_VERSION_APP_LABEL,
+                                    positive = com.simplemobiletools.commons.R.string.ok,
+                                    negative = 0
+                                ) {
+                                    launchViewIntent(DEVELOPER_PLAY_STORE_URL)
+                                }
+                            }
+                        ) {
+                            Text("Test button")
+                        }
+                    }
+                }
 
-        binding.mainColorCustomization.setOnClickListener {
-            startCustomizationActivity()
+                AppLaunched()
+            }
         }
-        binding.bottomSheetChooser.setOnClickListener {
-            launchAbout()
+    }
+
+    @Composable
+    private fun AppLaunched(
+        donateAlertDialogState: AlertDialogState = getDonateAlertDialogState(),
+        rateStarsAlertDialogState: AlertDialogState = getRateStarsAlertDialogState(),
+    ) {
+        LaunchedEffect(Unit) {
+            appLaunchedCompose(
+                appId = BuildConfig.APPLICATION_ID,
+                showDonateDialog = donateAlertDialogState::show,
+                showRateUsDialog = rateStarsAlertDialogState::show,
+                showUpgradeDialog = {}
+            )
         }
-        binding.manageBlockedNumbers.setOnClickListener {
-            startActivity(Intent(this, ManageBlockedNumbersActivity::class.java))
+    }
+
+    @Composable
+    private fun getDonateAlertDialogState() =
+        rememberAlertDialogState().apply {
+            DialogMember {
+                DonateAlertDialog(alertDialogState = this)
+            }
         }
-        binding.composeDialogs.setOnClickListener {
-            startActivity(Intent(this, TestDialogActivity::class.java))
+
+    @Composable
+    private fun getRateStarsAlertDialogState() = rememberAlertDialogState().apply {
+        DialogMember {
+            RateStarsAlertDialog(alertDialogState = this, onRating = ::rateStarsRedirectAndThankYou)
         }
-        binding.testButton.setOnClickListener {
-            ConfirmationDialog(this, FAKE_VERSION_APP_LABEL, positive = com.simplemobiletools.commons.R.string.ok, negative = 0) {
-               launchViewIntent(DEVELOPER_PLAY_STORE_URL)
-           }
+    }
+
+    private fun startCustomizationActivity() {
+        Intent(applicationContext, CustomizationActivity::class.java).apply {
+            putExtra(APP_ICON_IDS, getAppIconIDs())
+            putExtra(APP_LAUNCHER_NAME, getAppLauncherName())
+            startActivity(this)
         }
     }
 
@@ -74,27 +196,29 @@ class MainActivity : BaseSimpleActivity() {
         startAboutActivity(R.string.smtco_app_name, licenses, BuildConfig.VERSION_NAME, faqItems, true)
     }
 
-    private fun launchBottomSheetDemo() {
-        BottomSheetChooserDialog.createChooser(
-            fragmentManager = supportFragmentManager,
-            title = com.simplemobiletools.commons.R.string.please_select_destination,
-            items = arrayOf(
-                SimpleListItem(1, com.simplemobiletools.commons.R.string.record_video, com.simplemobiletools.commons.R.drawable.ic_camera_vector),
-                SimpleListItem(
-                    2,
-                    com.simplemobiletools.commons.R.string.record_audio,
-                    com.simplemobiletools.commons.R.drawable.ic_microphone_vector,
-                    selected = true
-                ),
-                SimpleListItem(4, com.simplemobiletools.commons.R.string.choose_contact, com.simplemobiletools.commons.R.drawable.ic_add_person_vector)
-            )
-        ) {
-            toast("Clicked ${it.id}")
+    private fun startAboutActivity(
+        appNameId: Int, licenseMask: Long, versionName: String, faqItems: ArrayList<FAQItem>, showFAQBeforeMail: Boolean,
+        getAppIconIDs: ArrayList<Int> = getAppIconIDs(),
+        getAppLauncherName: String = getAppLauncherName()
+    ) {
+        hideKeyboard()
+        Intent(applicationContext, AboutActivity::class.java).apply {
+            putExtra(APP_ICON_IDS, getAppIconIDs)
+            putExtra(APP_LAUNCHER_NAME, getAppLauncherName)
+            putExtra(APP_NAME, getString(appNameId))
+            putExtra(APP_LICENSES, licenseMask)
+            putExtra(APP_VERSION_NAME, versionName)
+            putExtra(APP_FAQ, faqItems)
+            putExtra(SHOW_FAQ_BEFORE_MAIL, showFAQBeforeMail)
+            startActivity(this)
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        setupToolbar(binding.mainToolbar)
+    private fun getAppLauncherName() = getString(R.string.smtco_app_name)
+
+    private fun getAppIconIDs(): ArrayList<Int> {
+        val ids = ArrayList<Int>()
+        ids.add(R.mipmap.commons_launcher)
+        return ids
     }
 }
