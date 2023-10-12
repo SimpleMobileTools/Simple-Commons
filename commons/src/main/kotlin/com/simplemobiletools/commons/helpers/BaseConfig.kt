d21f410494da08b35c563d6cf9f6610b9740fc91
FunkyMuse <kikoski@protonmail.com>
2023-10-12 22:45:02 +0200

    refactor: deprecated fun

--- commons/src/main/kotlin/com/simplemobiletools/commons/helpers/BaseConfig.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/helpers/BaseConfig.kt
@@ -4,18 +4,19 @@ import android.content.Context
 import android.content.res.Configuration
 import android.os.Environment
 import android.text.format.DateFormat
+import androidx.core.content.ContextCompat
 import com.simplemobiletools.commons.R
 import com.simplemobiletools.commons.extensions.getInternalStoragePath
 import com.simplemobiletools.commons.extensions.getSDCardPath
 import com.simplemobiletools.commons.extensions.getSharedPrefs
 import com.simplemobiletools.commons.extensions.sharedPreferencesCallback
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filterNotNull
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.LinkedList
 import java.util.Locale
 import kotlin.reflect.KProperty0
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filterNotNull
 
 open class BaseConfig(val context: Context) {
     protected val prefs = context.getSharedPrefs()
@@ -85,19 +86,19 @@ open class BaseConfig(val context: Context) {
     private fun getDefaultInternalPath() = if (prefs.contains(INTERNAL_STORAGE_PATH)) "" else context.getInternalStoragePath()
 
     var textColor: Int
-        get() = prefs.getInt(TEXT_COLOR, context.resources.getColor(R.color.default_text_color))
+        get() = prefs.getInt(TEXT_COLOR, ContextCompat.getColor(context, R.color.default_text_color))
         set(textColor) = prefs.edit().putInt(TEXT_COLOR, textColor).apply()
 
     var backgroundColor: Int
-        get() = prefs.getInt(BACKGROUND_COLOR, context.resources.getColor(R.color.default_background_color))
+        get() = prefs.getInt(BACKGROUND_COLOR, ContextCompat.getColor(context, R.color.default_background_color))
         set(backgroundColor) = prefs.edit().putInt(BACKGROUND_COLOR, backgroundColor).apply()
 
     var primaryColor: Int
-        get() = prefs.getInt(PRIMARY_COLOR, context.resources.getColor(R.color.default_primary_color))
+        get() = prefs.getInt(PRIMARY_COLOR, ContextCompat.getColor(context, R.color.default_primary_color))
         set(primaryColor) = prefs.edit().putInt(PRIMARY_COLOR, primaryColor).apply()
 
     var accentColor: Int
-        get() = prefs.getInt(ACCENT_COLOR, context.resources.getColor(R.color.default_accent_color))
+        get() = prefs.getInt(ACCENT_COLOR, ContextCompat.getColor(context, R.color.default_accent_color))
         set(accentColor) = prefs.edit().putInt(ACCENT_COLOR, accentColor).apply()
 
     var lastHandledShortcutColor: Int
@@ -105,14 +106,14 @@ open class BaseConfig(val context: Context) {
         set(lastHandledShortcutColor) = prefs.edit().putInt(LAST_HANDLED_SHORTCUT_COLOR, lastHandledShortcutColor).apply()
 
     var appIconColor: Int
-        get() = prefs.getInt(APP_ICON_COLOR, context.resources.getColor(R.color.default_app_icon_color))
+        get() = prefs.getInt(APP_ICON_COLOR, ContextCompat.getColor(context, R.color.default_app_icon_color))
         set(appIconColor) {
-            isUsingModifiedAppIcon = appIconColor != context.resources.getColor(R.color.color_primary)
+            isUsingModifiedAppIcon = appIconColor != ContextCompat.getColor(context, R.color.color_primary)
             prefs.edit().putInt(APP_ICON_COLOR, appIconColor).apply()
         }
 
     var lastIconColor: Int
-        get() = prefs.getInt(LAST_ICON_COLOR, context.resources.getColor(R.color.color_primary))
+        get() = prefs.getInt(LAST_ICON_COLOR, ContextCompat.getColor(context, R.color.color_primary))
         set(lastIconColor) = prefs.edit().putInt(LAST_ICON_COLOR, lastIconColor).apply()
 
     var customTextColor: Int
@@ -136,11 +137,11 @@ open class BaseConfig(val context: Context) {
         set(customAppIconColor) = prefs.edit().putInt(CUSTOM_APP_ICON_COLOR, customAppIconColor).apply()
 
     var widgetBgColor: Int
-        get() = prefs.getInt(WIDGET_BG_COLOR, context.resources.getColor(R.color.default_widget_bg_color))
+        get() = prefs.getInt(WIDGET_BG_COLOR, ContextCompat.getColor(context, R.color.default_widget_bg_color))
         set(widgetBgColor) = prefs.edit().putInt(WIDGET_BG_COLOR, widgetBgColor).apply()
 
     var widgetTextColor: Int
-        get() = prefs.getInt(WIDGET_TEXT_COLOR, context.resources.getColor(R.color.default_widget_text_color))
+        get() = prefs.getInt(WIDGET_TEXT_COLOR, ContextCompat.getColor(context, R.color.default_widget_text_color))
         set(widgetTextColor) = prefs.edit().putInt(WIDGET_TEXT_COLOR, widgetTextColor).apply()
 
     // hidden folder visibility protection
@@ -493,11 +494,11 @@ open class BaseConfig(val context: Context) {
     var colorPickerRecentColors: LinkedList<Int>
         get(): LinkedList<Int> {
             val defaultList = arrayListOf(
-                context.resources.getColor(R.color.md_red_700),
-                context.resources.getColor(R.color.md_blue_700),
-                context.resources.getColor(R.color.md_green_700),
-                context.resources.getColor(R.color.md_yellow_700),
-                context.resources.getColor(R.color.md_orange_700)
+                ContextCompat.getColor(context, R.color.md_red_700),
+                ContextCompat.getColor(context, R.color.md_blue_700),
+                ContextCompat.getColor(context, R.color.md_green_700),
+                ContextCompat.getColor(context, R.color.md_yellow_700),
+                ContextCompat.getColor(context, R.color.md_orange_700)
             )
             return LinkedList(prefs.getString(COLOR_PICKER_RECENT_COLORS, null)?.lines()?.map { it.toInt() } ?: defaultList)
         }
